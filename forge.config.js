958cf06d93a7069fbe2664971df82d201f092d5e
谢飞鸿 <xiefeihong@foxmail.com>
2023-05-05 19:34:43 +0800

    github workflows测试

--- forge.config.js
+++ forge.config.js
@@ -16,14 +16,14 @@ module.exports = {
       name: '@electron-forge/maker-deb',
       config: {},
     },
-    {
+    /*{
       name: '@electron-forge/maker-rpm',
       config: {
         options: {
           icon: 'public/icons/icon.png'
         },
       },
-    },
+    },*/
   ],
   publishers: [
     {
